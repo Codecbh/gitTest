8a8d0305eefc59d147d704d29deaae59fd24a323
cbh <it20150901@163.com>
2020-05-31 11:54:52 +0800

    创建java

---
+++ java/src/main.java
@@ -0,0 +1,13 @@
+/**
+ * @ProjectName: gitTest
+ * @Package: PACKAGE_NAME
+ * @ClassName: main
+ * @Author: Administrator
+ * @Description:
+ * @Date: 2020/5/31 11:51 * @Version: 1.0
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("123456");
+    }
+}
